3385480acb2090bc1173f9f682f54d63167bd569
Martín Gotelli Ferenaz <martingotelliferenaz@gmail.com>
2021-04-27 20:29:33 -0300

    Deleting main

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from services.dolar_si_api import DolarSiAPI
 
-"""if __name__ == '__main__':
-    with open('fernet_key.ini', 'r') as key_file:
-        fernet_key = bytes(key_file.read(), 'utf-8')
-
-    print(Fernet(fernet_key).encrypt(b'{"user": "mgotelli",'
-                               b'"password": "Kilombo6738"}'))
-"""
-
 
 class Stock:
     def __init__(self):
