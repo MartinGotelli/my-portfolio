865d4a828c85857a6508f2c236a570e97bf06ff7
Martín Gotelli Ferenaz <martingotelliferenaz@gmail.com>
2022-02-02 23:10:02 -0300

    Google auth error

--- services/credentials_manager.py
+++ services/credentials_manager.py
@@ -55,7 +55,6 @@ class CredentialsManager:
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
                     'credentials.json', scopes)
-                flow.redirect_uri = os.environ['REDIRECT_URL']
                 credentials = flow.run_local_server(port=0)
             # Save the credentials for the next run
             with open('token.json', 'w') as token:
